96f37b9ca6db76ccc3c203b99e58d542545b1994
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-17 22:16:37 +0800

    empty message

--- src/main/java/TaxiMeter.java
+++ src/main/java/TaxiMeter.java
@@ -21,7 +21,6 @@ public class TaxiMeter {
         BigDecimal totalPrice = valueOf(STARTING_PRICE).add(addPricePerMiles(miles - STARTING_PRICE_MILES));
         if (miles == 10) {
             totalPrice = totalPrice.add(longDistancePrice(miles - 8));
-            return 13;
         }
         return toInteger(totalPrice);
     }
