c1788bedfd4ef234d9e7e0c286cc073406cf2d6e
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-17 21:11:09 +0800

    empty message

--- src/main/java/TaxiMeter.java
+++ src/main/java/TaxiMeter.java
@@ -1,2 +1,9 @@
 public class TaxiMeter {
+    public TaxiMeter(int miles, int minutes) {
+
+    }
+
+    public Integer calculate() {
+        return 6;
+    }
 }
--- src/test/java/TaxiMeterTest.java
+++ src/test/java/TaxiMeterTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TaxiMeterTest {
     @Test
     void should_calculate_price_be_6_given_1_miles_and_0_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
+        TaxiMeter taxiMeter = new TaxiMeter(1,0);
+        assertThat(taxiMeter.calculate()).isEqualTo(6);
     }
 }
