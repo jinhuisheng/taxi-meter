34b33cd2feaee246e592f2159ebc6f6251dc937a
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-17 21:16:00 +0800

    empty message

--- src/main/java/TaxiMeter.java
+++ src/main/java/TaxiMeter.java
@@ -12,6 +12,10 @@ public class TaxiMeter {
         if (miles <= 2) {
             return 6;
         }
-        return 6 + 1;
+        return 6 + addPricePerMiles(1);
+    }
+
+    private int addPricePerMiles(int miles) {
+        return 1;
     }
 }
