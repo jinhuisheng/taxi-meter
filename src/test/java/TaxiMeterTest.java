d6aeaae9a5c62e790e7d56affa6ab6570a844d5f
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-17 21:09:44 +0800

    empty message

---
+++ src/test/java/TaxiMeterTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class TaxiMeterTest {
+    @Test
+    void should_calculate_price_be_6_given_1_miles_and_0_minute() {
+        TaxiMeter taxiMeter = new TaxiMeter();
+    }
+}
