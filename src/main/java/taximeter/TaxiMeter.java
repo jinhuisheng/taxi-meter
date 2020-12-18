6e9888c4695f59d23a08f35b80c57591705a9b70
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-18 18:17:17 +0800

    empty message

--- src/main/java/taximeter/TaxiMeter.java
+++ src/main/java/taximeter/TaxiMeter.java
@@ -5,12 +5,13 @@ import static java.lang.Math.round;
 public class TaxiMeter {
 
     private static final int STARTING_PRICE = 6;
+    public static final float PRICE_PER_MILES = 0.8f;
 
     public Integer charge(int miles, int minutes) {
         if (miles <= 2) {
             return STARTING_PRICE;
         }
-        float extraMilesPrice = (miles - 2) * 0.8f;
+        float extraMilesPrice = (miles - 2) * PRICE_PER_MILES;
         return round(STARTING_PRICE + extraMilesPrice);
     }
 }
