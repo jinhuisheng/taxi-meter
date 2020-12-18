6b78a8aef45e8fdbe22e645662dd60624c913115
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-18 18:42:26 +0800

    empty message

--- src/test/java/taximeter/TaxiMeterTest.java
+++ src/test/java/taximeter/TaxiMeterTest.java
@@ -1,13 +1,21 @@
 package taximeter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class TaxiMeterTest {
+
+    private TaxiMeter taxiMeter;
+
+    @BeforeEach
+    void setUp() {
+        taxiMeter = new TaxiMeter();
+    }
+
     @Test
     void should_charge_6_yuan_given_1_miles_and_0_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
         Integer charge = taxiMeter.charge(1, 0);
         assertThat(charge).isEqualTo(6);
     }
