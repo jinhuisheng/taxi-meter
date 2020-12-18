190a9d8dbe24892ec125713a11833e5397087102
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-18 18:29:08 +0800

    empty message

--- src/main/java/taximeter/TaxiMeter.java
+++ src/main/java/taximeter/TaxiMeter.java
@@ -6,16 +6,18 @@ public class TaxiMeter {
 
     private static final int STARTING_PRICE = 6;
     private static final float PRICE_PER_MILES = 0.8f;
+    public static final int TWO_MILES = 2;
+    public static final int EIGHT_MILES = 8;
 
     public Integer charge(int miles, int minutes) {
         float total = 0;
         total += STARTING_PRICE;
-        if (miles > 2) {
-            float extraMilesPrice = (miles - 2) * PRICE_PER_MILES;
+        if (miles > TWO_MILES) {
+            float extraMilesPrice = (miles - TWO_MILES) * PRICE_PER_MILES;
             total += extraMilesPrice;
         }
-        if (miles > 8) {
-            total += longDistancePrice(miles - 8);
+        if (miles > EIGHT_MILES) {
+            total += longDistancePrice(miles - EIGHT_MILES);
         }
         return round(total);
     }
