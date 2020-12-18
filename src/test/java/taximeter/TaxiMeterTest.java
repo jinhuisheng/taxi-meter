8f1cd778fbc92fe960e8c77929f9f5e1061f393d
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-18 17:55:07 +0800

    empty message

--- src/test/java/taximeter/TaxiMeterTest.java
+++ src/test/java/taximeter/TaxiMeterTest.java
@@ -1,34 +1,10 @@
 package taximeter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class TaxiMeterTest {
     @Test
-    void should_calculate_price_be_6_given_1_miles_and_0_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
-        Assertions.assertThat(taxiMeter.calculate(1, 0)).isEqualTo(6);
+    void should_charge_6_yuan_given_1_miles_and_0_minute() {
+        
     }
-
-    @Test
-    void should_calculate_price_be_7_given_3_miles_and_0_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
-        Assertions.assertThat(taxiMeter.calculate(3, 0)).isEqualTo(7);
-    }
-
-    @Test
-    void should_calculate_price_be_13_given_10_miles_and_0_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
-        Assertions.assertThat(taxiMeter.calculate(10, 0)).isEqualTo(13);
-    }
-
-    @Test
-    void should_calculate_price_be_7_given_2_miles_and_3_minute() {
-        TaxiMeter taxiMeter = new TaxiMeter();
-        Assertions.assertThat(taxiMeter.calculate(2, 3)).isEqualTo(7);
-    }
-
-
 }
